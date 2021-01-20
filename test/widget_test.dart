f57723c5de0bd8015946a31423483149eb1eaee3
Angel Alvarez Pérez <arperez454@gmail.com>
2021-01-19 23:01:32 -0500

    Delete external files.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,17 +5,14 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:cubaros/ui/Pages/Main/MenuPage.dart';
+import 'package:cubaros/ui/Pages/Main/menu_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:cubaros/app.dart';
-
-import 'package:cubaros/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(App());
+    await tester.pumpWidget(MenuPage());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
